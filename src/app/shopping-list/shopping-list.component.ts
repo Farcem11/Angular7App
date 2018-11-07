e45a6d3ba86400fd8c03f6aa8948697c348bd2fc
Fabian Arce <farce@creditsesame.com>
2018-11-06 22:26:46 -0600

    Add more components and models

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../share/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients : Ingredient[] = [
+    new Ingredient("Apple", 5),
+    new Ingredient("Cinnamon", 7)
+  ];
 
   constructor() { }
 
